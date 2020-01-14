fc4f19645b39390e5a1c15ea010de2e3a4d85f47
abnermarquesdeveloper <abnerdeveloper1@gmail.com>
2020-01-13 22:05:34 -0300

    Spring MVC

--- src/main/java/com/abnerdeveloper/projeto1/config/DataInitializer.java
+++ src/main/java/com/abnerdeveloper/projeto1/config/DataInitializer.java
@@ -22,11 +22,34 @@ public class DataInitializer implements ApplicationListener<ContextRefreshedEven
 		List<User> users = userRepository.findAll();
 		
 		if(users.isEmpty()) {
-			User user = new User();
-			user.setEmail("abner.marques@gmail.com.br");
-			user.setName("Abner L. Marques");
-			userRepository.save(user);
+			createUser("João da Silva", "Joaos@gmail.com");
+			createUser("Abner Lisboa Marques", "abner.marques@gmal.com");
+			createUser("Maria Aparecida", "mariaapa@gmail.com");
 		}
+		
+		//Pesquisa no banco por ID...
+		/*User user = userRepository.getOne(2L);
+		System.out.println(user.getName());*/
+		
+		//Apaga do banco um registro específico por ID...
+		//userRepository.deleteById(2L);
+		
+		//Altera um registro por ID...
+		/*User user = userRepository.getOne(1L);
+		user.setEmail("joaodasilva@gmail.com");
+		userRepository.save(user);*/
+		
+		//Busca com método customizado para busca pelo nome. é só definir o método na inteface que herda jpaRepository,
+		//e usar aqui...
+		//User user = userRepository.findByName("Maria Aparecida");
+		//System.out.println(user.getName());
+		User user = userRepository.findByEmail("abner.marques@gmal.com");
+		System.out.print(user.getEmail());
+	}
+	public void createUser(String name, String email) {
+		
+		User user = new User(name, email);
+		userRepository.save(user);
 	}
 
 }
--- src/main/java/com/abnerdeveloper/projeto1/repository/UserRepository.java
+++ src/main/java/com/abnerdeveloper/projeto1/repository/UserRepository.java
@@ -1,9 +1,16 @@
 package com.abnerdeveloper.projeto1.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.abnerdeveloper.projeto1.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long>{
 
+	//Pode criar uma Query customizada com a anotação @Query...
+	@Query("select u from User u where u.name like %?1%")
+	User findByName(String name);
+	
+	User findByEmail(String email);
+	
 }
